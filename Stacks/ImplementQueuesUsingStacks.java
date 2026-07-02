18110a55d9109e19f5ad92331f836e952d7d0f77
Lakshay Aneja <147088496+LAKSHAYANEJA@users.noreply.github.com>
2026-07-02 23:59:15 +0530

    LC_ImplementQueuesUsingStacks.java

---
+++ Stacks/ImplementQueuesUsingStacks.java
@@ -0,0 +1,49 @@
+class MyQueue {
+    Deque<Integer> s1;
+        Deque<Integer> s2;
+    public MyQueue() {
+        s1 = new ArrayDeque<>();
+        s2 = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        s1.push(x);
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+    
+    public int pop() {
+        if(s2.isEmpty()){
+        while(!s1.isEmpty()){
+        int value = s1.pop();
+        s2.push(value);
+    }
+        }
+        return s2.pop();
+        
+    }
+    
+    public int peek() {
+        if(s2.isEmpty()){
+        while(!s1.isEmpty()){
+            // int value = s1.peek();
+            
+            s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+    
+   
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
