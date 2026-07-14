e12c807be729e6cb20f840630ea97a6a51df7288
Lakshay Aneja <147088496+LAKSHAYANEJA@users.noreply.github.com>
2026-07-14 16:18:45 +0530

    LC_901_OnlineStockSpan.java

---
+++ Stacks/OnlineStockSpan.java
@@ -0,0 +1,35 @@
+class Pair{
+    int price;
+    int span;
+
+    Pair(int price, int span){
+        this.price = price;
+        this.span = span;
+    }
+}
+
+class StockSpanner {
+
+    Deque<Pair> stack;
+
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+    while(!stack.isEmpty() && stack.peek().price <= price){
+        span += stack.peek().span;
+        stack.pop();
+    }
+    stack.push(new Pair(price, span));
+
+    return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
